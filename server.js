3199bb49ab945eed5c26853f8123fa8d6f162d08
Victor Lia Fook <victorliafook@gmail.com>
2017-03-24 04:55:58 +0000

    bug on timestamps - milliseconds instead of seconds. Final adjustment

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 var express = require('express');
 
-
+console.log(process.env.PORT);
 var app = express();
+//JS works with millisseconds in their Date methods. Unix time is in seconds.
 app.get('/:par', function(req, res){
    
     var time = new TimeObj();
     var date = Date.parse(req.params.par);
     
     if(!isNaN(date)){
-        time.unix = date;
+        time.unix = date / 1000;
         time.natural = new Date(date).toDateString();
     }else{
-        date = new Date(parseInt(req.params.par));
+        date = new Date(parseInt(req.params.par * 1000));
         if(!isNaN( date.getTime())){
-            time.unix = date.getTime()/1000;
+            time.unix = date.getTime() / 1000;
             time.natural = date.toDateString();
         }
     }
